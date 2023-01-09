d41e40835ad1e65ab3b90d83d6a7fcaf3b8b771b
Steven Gragg <sgragg5619@gmail.com>
2023-01-09 18:25:30 +0800

    Finalize about section

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,12 @@ type Props = {};
 
 export default function About({}: Props) {
   return (
-    <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-theme1 text-2xl">
         About
       </h3>
@@ -17,17 +22,17 @@ export default function About({}: Props) {
         className="-mb-20 md:mb-0 flex-shrink-0w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-[500px] xl:h-[500px]"
       />
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold text-theme1">
+        <h4 className="text-3xl font-semibold text-theme4">
           A <span className="underline decoration-theme1/50">little</span> bit
           about myself.
         </h4>
-        <p className="text-theme4">
+        <p className="text-theme4 text-sm">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
           sagittis eros quis luctus lobortis. Proin hendrerit porttitor nulla,
           sit amet sagittis ipsum. Class aptent taciti sociosqu ad litora
           torquent per conubia nostra, per inceptos himenaeos.
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 }
